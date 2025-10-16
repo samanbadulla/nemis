d527e4b04f46b55b4642a2cde305973b7dc4a31f
smadu <madusanka.app.online@gmail.com>
2025-10-16 10:33:34 +0530

    UserFactory update

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,13 +23,16 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $nic = $this->faker->unique()->numerify('#########V');
+
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'nic' => $this->faker->unique()->numerify('#########V'),
+            'nic' => $nic,
+            'nic_hash'       => hash('sha256', $nic), // hash the NIC
         ];
     }
 
@@ -38,7 +41,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
