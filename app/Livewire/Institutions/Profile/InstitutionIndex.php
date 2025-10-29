326a6bbc70d5fe55ebe1ee1955a4d1c732affd7e
phazilosman <31217090+phazilosman@users.noreply.github.com>
2025-10-29 15:47:08 +0530

    Fix

---
+++ app/Livewire/Institutions/Profile/InstitutionIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Institutions\Profile;
+
+use Livewire\Component;
+
+class InstitutionIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.institutions.profile.institution-index');
+    }
+}
